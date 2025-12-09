cc532e0e1ba0f5fc80147e2261f6270637ef874a
Josue Perez <josuepe03@hotmail.com>
2025-12-09 15:53:59 -0600

    fixed: livewire con

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,8 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script> 
+
 </head>
 <body class="bg-gray-50 font-sans text-gray-800">
 
